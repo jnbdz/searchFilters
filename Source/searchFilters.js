f1f96fb598031d8206563dbe7ba8f4e54da13544
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2011-04-26 02:25:29 -0400

    updated the script to a one that is working

--- Source/searchFilters.js
+++ Source/searchFilters.js
@@ -16,111 +16,107 @@ provides:
 ...
 */
 
-var searchFilters = Class({
+var searchFilters = new Class({
 
-        Implements: [Options, Events],
-
-        options: {
-                max: 50,
-                filters: {},
-                action: ''
-        },
-
-        initialize: function(el, options){
-
-                this.element = document.id(el);
-                this.setOptions(options);
-
-                this.numberOfFiltersRow = 0;
-                this.construct();
-
-        },
-
-        construct: function(){
-
-                Object.each(this.options.filters, function(columns, formName){
-
-                        this.filtersColumns = columns;
-                        this.formName = formName;
-
+    Extends: listManager,
+    
+    Implements: [Options, Events],
+    
+    options: {
+        max: 50,
+        filters: {},
+        action: ''
+    },
+    
+    initialize: function(el, options){
+        
+        this.element = document.id(el);
+        this.setOptions(options);
+        
+        this.numberOfFiltersRow = 0;
+        
+        this.filtersWrapper = this.construct();
+        
+        this.parent(this.filtersWrapper, options);
+        
+    },
+    
+    construct: function(){
+            
+    return new Element('ul').inject(new Element('form', {
+            'action': this.options.action,
+            'name': 'searchfilters',
+            'method': 'get'
+        }).inject(this.element, 'top'));
+        
+    },
+    
+    add: function(rowOptions){
+   
+        if (this.numberOfFiltersRow > this.options.max) return;
+       
+        var option;
+        var options = [];
+        
+        var clickOnOption = function(configs) {return {'click': function(el){
+            this.addFilterControls(this.getRow(el).getChildren('.searchfilters-filtercontrolsel'), configs.type);
+            }.bind(this)
+           };
+        }.bind(this);
+        
+        Object.each(this.options.filters, function(configs, value){
+           
+            option = new Element('option', {
+                'value': value,
+                'text': configs.title,
+                'class': 'searchfilters-row-option-filtertype-' + configs.type,
+                'events': clickOnOption(configs)
                 });
+            
+            options.include(option);
+  
+          });
+        
+        var select = [new Element('select', {}).adopt(options), Element('div', {'class': 'searchfilters-filtercontrolsel'})];
+        
+        rowOptions.rowHTML = rowOptions.rowHTML.combine(select);
+        
+        this.parent(rowOptions);
+    
+        this.numberOfFiltersRow++;
+        
+    },
 
-                this.filtersWrapEl =  new Element('form', {
-                        'action': this.options.action,
-                        'name': 'searchfilters',
-                        'method': 'get'
-                }).adopt(new Element('input', {
-                        'type': 'hidden',
-                        'name': 'searchfilters-formname',
-                        'value': this.formName
-                })).insert(this.element);
+    addFilterControls: function(el, type){
 
-        },
+        el.set('html', '');
 
-        add: function(){
+        var customFilter = this.fireEvent('addingFilterControls', [type]);
+        
+        var filterControls;
+        
+        switch(type){
+                case 'date':
+                    filterControls = this.filterType_date(el);
+                break;
+                case 'bool':
+                    filterControls = this.filterType_bool(el);
+                break;
+                case 'text':
+                    filterControls = this.filterType_text(el);
+                break;
+                default:
+                    filterControls = el.adopt();
+                break;
+        }
+        
+        this.fireEvent('addedFilterControls', [type, filterControls]);
+        
+    },
+    
+    filterType_date: function(el){
 
-                if (this.numberOfFiltersRow > this.options.max) return;
-
-                var option = null;
-                var options = [];
-
-                Object.each(this.filtersColumns, function(values, column){
-
-                        option = new Element('option', function(){
-                                'value': column,
-                                'text': values.title,
-                                'class': 'searchfilters-row-columns-list-option-filtertype-' + values.type
-                        });
-
-                        options.include(option);
-
-                });
-
-                new Element('div', {
-                        'class': 'searchfilters-row'
-                }).adopt(
-                        new Element('select', {'name': 'searchfilters-select-column-name-' + this.numberOfFiltersRow, 'class': 'searchfilters-row-columns-list'}).adopt(options),
-                        new Element('div', {'class': 'searchfilters-row-columnoptions'}),
-                        new Element('a', {'text': '-', 'class': 'searchfilters-row-remove'}).addEvent('click', this.remove().bind(this)),
-                        new Element('a', {'text': '+', 'class': 'searchfilters-row-add'}).addEvent('click', this.add().bind(this))
-                ).insert(this.filtersWrapEl);
-
-                this.numberOfFiltersRow = (this.numberOfFiltersRow + 1);
-
-        },
-
-        remove: function(){
-
-                this.getParent('div.searchfilters-row').destroy();
-
-                return this.numberOfFiltersRow = (this.numberOfFiltersRow - 1);
-
-        },
-
-        filterType_text: function(){
-
-                var filterType_text_Select = new Element('select', {
-                        'name': 'searchfilters-select-column-filtertype-text-select-' + this.numberOfFiltersRow
-                }).adopt(
-                        new Element('option', {'text': 'matches', 'value': 'matches', 'selected': 'selected'}),
-                        new Element('option', {'text': 'contains', 'value': 'contains'}),
-                        new Element('option', {'text': 'begins with', 'value': 'begins-with'}),
-                        new Element('option', {'text': 'ends with', 'value': 'ends-with'}),
-                        new Element('option', {'text': 'is', 'value': 'is'})
-                );
-
-                var filterType_text_Input = new Element('input', {
-                        'type': 'text',
-                        'name': 'searchfilters-select-column-filtertype-text-input-' + this.numberOfFiltersRow,
-                        'value': ''
-                });
-
-                return [filterType_text_Select, filterType_text_Input];
-
-        },
-
-        filterType_date: function(){
-
+        el.adopt([
                 new Element('select').adopt(
                         new Element('option', {'text': 'within last', 'value': 'within-last'}),
                         new Element('option', {'text': 'exactly', 'value': 'exactly'}),
@@ -131,25 +127,72 @@ var searchFilters = Class({
                         new Element('option', {'text': 'this week', 'value': 'this-week'}),
                         new Element('option', {'text': 'this month', 'value': 'this-month'}),
                         new Element('option', {'text': 'this year', 'value': 'this-year'})
-                );
+                ),
 
-                new Element('input', {'type': 'text'});
+                new Element('input', {'type': 'text'}),
 
                 new Element('select').adopt(
                         new Element('option', {'text': 'days', 'value': 'days'}),
                         new Element('option', {'text': 'weeks', 'value': 'weeks'}),
                         new Element('option', {'text': 'months', 'value': 'months'}),
                         new Element('option', {'text': 'years', 'value': 'years'})
-                );
+                )
+        ]);
 
-        },
+    },
 
-        filterType_bool: function(){
+    filterType_bool: function(el){
 
-                new Element('input', {
-                        'type': 'checkbox'
-                });
+        el.adopt(new Element('input', {
+            'type': 'checkbox'
+        }));
 
-        }
+    },
+    
+    filterType_text: function(el){
 
+        el.adopt([
+                    
+            new Element('select', {
+                'name': 'searchfilters-select-filtertype-text-select-' + this.numberOfFiltersRow
+            }).adopt(
+                new Element('option', {'text': 'matches', 'value': 'matches', 'selected': 'selected'}),
+                new Element('option', {'text': 'contains', 'value': 'contains'}),
+                new Element('option', {'text': 'begins with', 'value': 'begins-with'}),
+                new Element('option', {'text': 'ends with', 'value': 'ends-with'}),
+                new Element('option', {'text': 'is', 'value': 'is'})
+            ),
+
+            new Element('input', {
+                'type': 'text',
+                'name': 'searchfilters-select-filtertype-text-input-' + this.numberOfFiltersRow,
+                'value': ''
+            })
+                    
+        ]);
+
+    },
+    
+    remove: function(el){
+
+        this.parent(this.getRow(el));
+        
+        this.numberOfFiltersRow--;
+    
+    },
+    
+    removeAll: function(){
+    
+        this.parent();
+        
+        this.numberOfFiltersRow = 0;
+
+    },
+    
+    getRow: function(el){
+
+        return document.id(el.target).getParent('.listmanager-row');
+        
+    }
+    
 });
